c8495ae06d16837351afd1d2a3fa70911ace4f6d
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-05-11 15:34:57 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,114 +1,26 @@
-import React, {Component} from "react";
-import {Container, Grid, Header} from "semantic-ui-react";
-import MenuBar from "./MenuComponent";
-import FormComponent from "./FormComponent";
-import OctalOutput from "./OctalOutput";
-import SymbolicOutput from "./SymbolicOutput";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      permissions: {
-        special: {
-          setuid: false,
-          setgid: false,
-          stickyMode: false
-        },
-        user: {
-          read: false,
-          write: false,
-          execute: false
-        },
-        group: {
-          read: false,
-          write: false,
-          execute: false
-        },
-        other: {
-          read: false,
-          write: false,
-          execute: false
-        }
-      }
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClear = this.handleClear.bind(this);
-  }
-
-  handleChange(triad, permission, value) {
-    this.setState(prevState => {
-      let updatedTriad = Object.assign({}, prevState.permissions[triad], {[permission]: value});
-      let updatedPermissions = Object.assign({}, prevState.permissions, {[triad]: updatedTriad});
-      return {
-        permissions: updatedPermissions
-      };
-    });
-  }
-
-  handleClear() {
-    this.setState({
-      permissions: {
-        special: {
-          setuid: false,
-          setgid: false,
-          stickyMode: false
-        },
-        user: {
-          read: false,
-          write: false,
-          execute: false
-        },
-        group: {
-          read: false,
-          write: false,
-          execute: false
-        },
-        other: {
-          read: false,
-          write: false,
-          execute: false
-        }
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <MenuBar/>
-        <div style={{marginTop: "4em"}}>
-          <Header as="h1" textAlign="center">Unix Permissions Calculator</Header>
-          <Container textAlign="center">
-            <p>
-              Check the required permissions in the following table and the octal and symbolic notations will be updated
-              accordingly.
-            </p>
-          </Container>
-          <Grid centered padded stackable>
-            <Grid.Row>
-              <Grid.Column width={10}>
-                <FormComponent permissions={this.state.permissions} onChange={this.handleChange}
-                               onClear={this.handleClear}/>
-              </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row columns={3}>
-              <Grid.Column>
-                <OctalOutput permissions={this.state.permissions}/>
-              </Grid.Column>
-
-              <Grid.Column>
-                <SymbolicOutput permissions={this.state.permissions}/>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
