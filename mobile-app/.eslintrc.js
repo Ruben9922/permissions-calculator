2343a92dc389a169386954093d083081e0cee50b
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-01-05 12:27:24 +0000

    Implement initial working app

--- mobile-app/.eslintrc.js
+++ mobile-app/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+
+  rules: {
+    quotes: ["error", "double"],
+  },
 };
--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import CheckBox from "@react-native-community/checkbox";
+import React from "react";
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -15,84 +9,277 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import {useImmer} from "use-immer";
+import startCase from "lodash/fp/startCase";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type Class = "special" | "user" | "group" | "other";
+type Permission = "read" | "write" | "execute";
+type SpecialPermission = "setuid" | "setgid" | "stickyMode";
+type Permissions = Omit<
+  Record<Class, Record<Permission, boolean>>,
+  "special"
+> & {special: Record<SpecialPermission, boolean>};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function createPermissions(value: boolean): Permissions {
+  const specialClass: Class = "special";
+  const normalClasses: Class[] = ["user", "group", "other"];
+  const normalPermissionArray: Permission[] = ["read", "write", "execute"];
+  const specialPermissionArray: SpecialPermission[] = ["setuid", "setgid", "stickyMode"];
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  const specialPermissions: Record<typeof specialClass, Record<SpecialPermission, boolean>> = {
+    special: Object.assign({}, ...specialPermissionArray.map(permission => ({[permission]: value}))),
+  };
+
+  const normalPermissions: Partial<Omit<Record<Class, Record<Permission, boolean>>, "special">>[] = normalClasses
+    .map<[Class, Permission[]]>($class => [$class, normalPermissionArray])
+    .map<[Class, Record<Permission, boolean>]>(([$class, permissionArray]) => (
+      [$class, Object.assign({}, ...permissionArray.map(permission => ({[permission]: value})))]
+    ))
+    .map<Partial<Omit<Record<Class, Record<Permission, boolean>>, "special">>>(([$class, classPermissions]) => ({
+      [$class]: classPermissions,
+    }));
+
+  return Object.assign({}, specialPermissions, ...normalPermissions);
 }
 
+const permissionsNoneSelected: Permissions = createPermissions(false);
+const permissionsAllSelected: Permissions = createPermissions(true);
+
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [permissions, updatePermissions] = useImmer(permissionsNoneSelected);
+
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const handleSpecialChange = (
+    permission: SpecialPermission,
+    value: boolean,
+  ): void => {
+    updatePermissions(draft => {
+      draft.special[permission] = value;
+    });
+  };
+
+  const handleChange = (
+    $class: Class,
+    permission: Permission,
+    value: boolean,
+  ): void => {
+    updatePermissions(draft => {
+      if ($class !== "special") {
+        draft[$class][permission] = value;
+      }
+    });
+  };
+
+  const handleSelectAll = (): void => updatePermissions(permissionsAllSelected);
+  const handleDeselectAll = (): void => updatePermissions(permissionsNoneSelected);
+
+  const allDeselected = Object.values(permissions)
+    .flatMap(classPermissions => Object.values(classPermissions))
+    .every(x => !x);
+
+  const allSelected = Object.values(permissions)
+    .flatMap(classPermissions => Object.values(classPermissions))
+    .every(x => x);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={styles.sectionTitle}>Unix Permissions Calculator</Text>
+          <Text style={styles.sectionTitle}>
+            Check the required permissions and the octal and symbolic notations
+            will be updated accordingly.
+          </Text>
+          <ScrollView horizontal={true}>
+            <Form
+              permissions={permissions}
+              handleSpecialChange={handleSpecialChange}
+              handleChange={handleChange}
+            />
+          </ScrollView>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Select all"
+              onPress={handleSelectAll}
+              disabled={allSelected}
+            />
+            <Button
+              title="Deselect all"
+              onPress={handleDeselectAll}
+              disabled={allDeselected}
+            />
+          </View>
+          <OctalOutput permissions={permissions} />
+          <SymbolicOutput permissions={permissions} />
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+}
+
+type CheckBoxWithTextProps = {
+  value: boolean;
+  text: string;
+  onValueChange?: (value: boolean) => void;
+};
+
+function CheckBoxWithText({
+  value,
+  text,
+  onValueChange,
+}: CheckBoxWithTextProps): React.JSX.Element {
+  return (
+    <View style={checkBoxStyles.checkBoxContainer}>
+      <CheckBox value={value} onValueChange={onValueChange} />
+      <Text style={checkBoxStyles.label}>{text}</Text>
+    </View>
+  );
+}
+
+type FormProps = {
+  permissions: Permissions;
+  handleSpecialChange: (permission: SpecialPermission, value: boolean) => void;
+  handleChange: ($class: Class, permission: Permission, value: boolean) => void;
+};
+
+function Form({
+  permissions,
+  handleSpecialChange,
+  handleChange,
+}: FormProps): React.JSX.Element {
+  return (
+    <View style={formStyle.container}>
+      {Object.entries(permissions).map(([$class, classPermissions]) => (
+        <View style={formStyle.column} key={$class}>
+          <Text>{startCase($class)}</Text>
+          {Object.entries(classPermissions).map(([permission, value]) => {
+            const onValueChange = (updatedValue: boolean) =>
+              $class === "special"
+                ? handleSpecialChange(
+                    permission as SpecialPermission,
+                    updatedValue,
+                  )
+                : handleChange(
+                    $class as Class,
+                    permission as Permission,
+                    updatedValue,
+                  );
+
+            return (
+              <CheckBoxWithText
+                key={permission}
+                value={value}
+                text={startCase(permission)}
+                onValueChange={onValueChange}
+              />
+            );
+          })}
+        </View>
+      ))}
+    </View>
+  );
+}
+
+type OctalOutputProps = {
+  permissions: Permissions;
+};
+
+function OctalOutput({permissions}: OctalOutputProps): React.JSX.Element {
+  const computeDigit = (read: boolean, write: boolean, execute: boolean): number => {
+    let digit = 0;
+    if (read) {
+      digit += 4;
+    }
+    if (write) {
+      digit += 2;
+    }
+    if (execute) {
+      digit += 1;
+    }
+    return digit;
+  };
+
+  const octal =
+    String(computeDigit(permissions.special.setuid, permissions.special.setgid, permissions.special.stickyMode))
+    + String(computeDigit(permissions.user.read, permissions.user.write, permissions.user.execute))
+    + String(computeDigit(permissions.group.read, permissions.group.write, permissions.group.execute))
+    + String(computeDigit(permissions.other.read, permissions.other.write, permissions.other.execute));
+
+  return (
+    <View>
+      <Text>Octal</Text>
+      <Text>{octal}</Text>
+      <Text>
+        Example: <Text>chmod {octal} /path/to/file</Text>
+      </Text>
+    </View>
+  );
+}
+
+type SymbolicOutputProps = {
+  permissions: Permissions;
+};
+
+function SymbolicOutput({permissions}: SymbolicOutputProps): React.JSX.Element {
+  const computeReadSymbol = (read: boolean): string => (read ? "r" : "-");
+  const computeWriteSymbol = (write: boolean): string => (write ? "w" : "-");
+  const computeExecuteSymbol = (execute: boolean, special: boolean, stickyMode: boolean): string => {
+    if (execute) {
+      if (special) {
+        if (stickyMode) {
+          return "t";
+        } else {
+          return "s";
+        }
+      } else {
+        return "x";
+      }
+    } else {
+      if (special) {
+        if (stickyMode) {
+          return "T";
+        } else {
+          return "S";
+        }
+      } else {
+        return "-";
+      }
+    }
+  };
+
+  const computeSymbolicForTriad = (classPermissions: Record<Permission, boolean>, special: boolean, stickyMode: boolean): string => {
+    const readSymbol = computeReadSymbol(classPermissions.read);
+    const writeSymbol = computeWriteSymbol(classPermissions.write);
+    const executeSymbol = computeExecuteSymbol(classPermissions.execute, special, stickyMode);
+
+    return readSymbol + writeSymbol + executeSymbol;
+  };
+
+  const symbolic = computeSymbolicForTriad(permissions.user, permissions.special.setuid, false)
+    + computeSymbolicForTriad(permissions.group, permissions.special.setgid, false)
+    + computeSymbolicForTriad(permissions.other, permissions.special.stickyMode, true);
+
+  return (
+    <View>
+      <Text>Symbolic Notation</Text>
+      <Text>{symbolic}</Text>
+    </View>
   );
 }
 
@@ -103,15 +290,39 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+});
+
+const checkBoxStyles = StyleSheet.create({
+  checkBoxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  label: {
+    margin: 0,
+  },
+});
+
+const formStyle = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  column: {
+    flexDirection: "column",
+    flex: 1,
   },
 });
 
