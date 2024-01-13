7f5992479ae15fe833a17dedcc6eac004a199947
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-01-13 15:40:24 +0000

    Add dark & light mode support for custom theme

--- mobile-app/index.js
+++ mobile-app/index.js
@@ -3,13 +3,13 @@
  */
 
 import React from "react";
-import {AppRegistry} from "react-native";
+import { AppRegistry, useColorScheme } from "react-native";
 import {MD3DarkTheme as DefaultTheme, PaperProvider} from "react-native-paper";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import App from "./App";
 import {name as appName} from "./app.json";
 
-const customTheme = {
+const customDarkTheme = {
   colors: {
     primary: "rgb(165, 200, 255)",
     onPrimary: "rgb(0, 49, 95)",
@@ -54,12 +54,60 @@ const customTheme = {
   },
 };
 
-const theme = {
-  ...DefaultTheme,
-  colors: customTheme.colors,
+const customLightTheme = {
+  colors: {
+    primary: "rgb(0, 95, 175)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(212, 227, 255)",
+    onPrimaryContainer: "rgb(0, 28, 58)",
+    secondary: "rgb(84, 95, 113)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(216, 227, 248)",
+    onSecondaryContainer: "rgb(17, 28, 43)",
+    tertiary: "rgb(110, 86, 118)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(247, 216, 255)",
+    onTertiaryContainer: "rgb(39, 20, 48)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(253, 252, 255)",
+    onBackground: "rgb(26, 28, 30)",
+    surface: "rgb(253, 252, 255)",
+    onSurface: "rgb(26, 28, 30)",
+    surfaceVariant: "rgb(224, 226, 236)",
+    onSurfaceVariant: "rgb(67, 71, 78)",
+    outline: "rgb(116, 119, 127)",
+    outlineVariant: "rgb(195, 198, 207)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(47, 48, 51)",
+    inverseOnSurface: "rgb(241, 240, 244)",
+    inversePrimary: "rgb(165, 200, 255)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(240, 244, 251)",
+      level2: "rgb(233, 239, 249)",
+      level3: "rgb(225, 235, 246)",
+      level4: "rgb(223, 233, 245)",
+      level5: "rgb(218, 230, 244)",
+    },
+    surfaceDisabled: "rgba(26, 28, 30, 0.12)",
+    onSurfaceDisabled: "rgba(26, 28, 30, 0.38)",
+    backdrop: "rgba(45, 49, 56, 0.4)",
+  },
 };
 
 export default function Main() {
+  const colorScheme = useColorScheme();
+  const isDarkMode = !colorScheme || colorScheme === "dark";
+
+  const theme = {
+    ...DefaultTheme,
+    colors: isDarkMode ? customDarkTheme.colors : customLightTheme.colors,
+  };
+
   return (
     <PaperProvider theme={theme}>
       <SafeAreaProvider>
